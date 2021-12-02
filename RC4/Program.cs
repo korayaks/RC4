8bb76232ae87b2480f10adf4a04218386e2f56a9
Koray Aksoy <burakkorayaksoy@hotmail.com>
2021-12-03 02:35:54 +0300

    initial

---
+++ RC4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace RC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText = "rc4 algorithm";
+            string key = "korayaks";
+            var encrypted = RC4.Encrypt(key, plainText);
+            foreach (var item in encrypted)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            var decrypted = RC4.Decrypt(key, encrypted);
+            char[] cipherText = new char[decrypted.Length];
+            for (int i = 0; i < decrypted.Length; i++)
+            {
+                cipherText[i] = Convert.ToChar(decrypted[i]);
+            }
+            foreach (var item in cipherText)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RC4/RC4.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RC4
+{
+    public static class RC4
+    {
+        public static byte[] Encrypt(string key, string plainText)
+        {
+            byte[] byteKey = Encoding.ASCII.GetBytes(key);
+            byte[] bytePlainText = Encoding.ASCII.GetBytes(plainText);
+            return (Encrypt(byteKey, bytePlainText));
+        }
+
+        public static byte[] Decrypt(string key, byte[] plainText)
+        {
+            byte[] byteKey = Encoding.ASCII.GetBytes(key);
+            return Encrypt(byteKey, plainText);
+        }
+
+        public static byte[] Encrypt(byte[] key, byte[] data)
+        {
+            return EncryptOutput(key, data).ToArray();
+        }
+
+        public static byte[] Decrypt(byte[] key, byte[] data)
+        {
+            return EncryptOutput(key, data).ToArray();
+        }
+
+        private static byte[] EncryptInitalize(byte[] key)
+        {
+            byte[] s = Enumerable.Range(0, 256)
+              .Select(i => (byte)i)
+              .ToArray();
+
+            for (int i = 0, j = 0; i < 256; i++)
+            {
+                j = (j + key[i % key.Length] + s[i]) & 255;
+
+                Swap(s, i, j);
+            }
+
+            return s;
+        }
+
+        private static IEnumerable<byte> EncryptOutput(byte[] key, IEnumerable<byte> data)
+        {
+            byte[] s = EncryptInitalize(key);
+
+            int i = 0;
+            int j = 0;
+
+            return data.Select((b) =>
+            {
+                i = (i + 1) & 255;
+                j = (j + s[i]) & 255;
+
+                Swap(s, i, j);
+
+                return (byte)(b ^ s[(s[i] + s[j]) & 255]);
+            });
+        }
+
+        private static void Swap(byte[] s, int i, int j)
+        {
+            byte c = s[i];
+
+            s[i] = s[j];
+            s[j] = c;
+        }
+    }
+}
